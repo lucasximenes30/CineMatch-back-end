7b2dfd898695abb81110b221ca5d1534a1100785
lucasximenes30 <lucasximenes177@gmail.com>
2026-03-06 20:00:19 -0300

    first commit

---
+++ src/main/java/com/cinematch/lucasximenes30/CineMatchApplication.java
@@ -0,0 +1,13 @@
+package com.cinematch.lucasximenes30;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineMatchApplication.class, args);
+	}
+
+}
